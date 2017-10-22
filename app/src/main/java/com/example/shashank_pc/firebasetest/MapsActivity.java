fa3ac734199efbc76937214fde035de3b4f853c8
Shashank Anantharam <shashank95.a@gmail.com>
2017-10-22 18:27:47 +0530

    Made a lot of changed

--- app/src/main/java/com/example/shashank_pc/firebasetest/MapsActivity.java
+++ app/src/main/java/com/example/shashank_pc/firebasetest/MapsActivity.java
@@ -9,9 +9,14 @@ import android.graphics.PorterDuff;
 import android.graphics.PorterDuffXfermode;
 import android.graphics.Rect;
 import android.graphics.RectF;
+import android.os.Handler;
+import android.support.annotation.NonNull;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
+import com.firebase.ui.storage.images.FirebaseImageLoader;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -22,10 +27,21 @@ import com.google.android.gms.maps.model.CircleOptions;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.io.ByteArrayOutputStream;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    private FirebaseStorage storage;
+    private StorageReference ref;
+    private Bitmap img;
+    private Marker marker;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,6 +51,10 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        storage = FirebaseStorage.getInstance();
+
+
     }
 
 
@@ -53,20 +73,118 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
         // Add a marker in Sydney and move the camera
         LatLng sydney = new LatLng(-34, 151);
-        Marker marker= mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        marker= mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
 
-        Bitmap img = BitmapFactory.decodeResource(getResources(),R.drawable.future_city);
+        ref= storage.getReference().child("Profile pics/9701420818.jpg");
+
+        img=null;
+/*
+        Upload
+        StorageReference ref2 = storage.getReference().child("Profile pics/city.jpg");
+
+        Bitmap dr_image= BitmapFactory.decodeResource(getResources(),R.drawable.future_city);
+
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        dr_image.compress(Bitmap.CompressFormat.JPEG, 100, baos);
+        byte[] data = baos.toByteArray();
+        UploadTask uploadTask= ref2.putBytes(data);
+        uploadTask.addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+
+            }
+        });
+        uploadTask.addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(getApplicationContext(),e.getMessage(),Toast.LENGTH_SHORT).show();
+            }
+        });
 
 
-        Bitmap small_img=Bitmap.createScaledBitmap(img, 120, 120, false);
+*/
 
-        //       Bitmap round_img= getRoundedRectBitmap(img);
-        //      Bitmap round_img= getCircleBitmap(img);
+/*        Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
 
-        marker.setIcon(BitmapDescriptorFactory.fromBitmap(small_img));
+                Toast.makeText(getApplicationContext(),"Running",Toast.LENGTH_SHORT).show();
+                try {
+                    img = Glide.with(getApplicationContext())
+                            .using(new FirebaseImageLoader())
+                            .load(ref)
+                            .asBitmap()
+                            .into(120, 120).get();
+                }
+                catch(Exception e)
+                {
+                    Toast.makeText(getApplicationContext(),e.getMessage(),Toast.LENGTH_SHORT).show();
+                }
 
-     //   mMap.addCircle(new CircleOptions().fillColor(R.drawable.other).center(sydney).radius(15.00));
+
+
+            }
+        };
+
+        Handler handler= new Handler();
+        handler.postDelayed(runnable,1000);
+*/
+
+
+
+        final long ONE_MEGABYTE = 1024 * 1024;
+        ref.getBytes(ONE_MEGABYTE).addOnSuccessListener(new OnSuccessListener<byte[]>() {
+            @Override
+            public void onSuccess(byte[] bytes) {
+
+                Toast.makeText(getApplicationContext(),"Downloaded",Toast.LENGTH_SHORT).show();
+
+
+                try
+                {
+                    img = BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
+
+                    Bitmap small_img = Bitmap.createScaledBitmap(img, 120, 120, false);
+
+                    Bitmap round_img= getRoundedRectBitmap(small_img);
+
+                    marker.setIcon(BitmapDescriptorFactory.fromBitmap(round_img));
+
+                    //Bitmap round_img= getCircleBitmap(img);
+                    marker.setAnchor(0.5f,0.5f);
+
+                }
+                catch (Exception e)
+                {
+
+                }
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception exception) {
+                // Handle any errors
+
+                Toast.makeText(getApplicationContext(), exception.getMessage(),Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+
+/*  Code for putting image im marker
+       if(img!=null) {
+            Bitmap small_img = Bitmap.createScaledBitmap(img, 120, 120, false);
+
+//        Bitmap round_img= getRoundedRectBitmap(small_img);
+            //      Bitmap round_img= getCircleBitmap(img);
+
+            marker.setIcon(BitmapDescriptorFactory.fromBitmap(small_img));
+        }
+
+//        Bitmap img = BitmapFactory.decodeResource(getResources(),R.drawable.future_city);
+
+*/
+
     }
 
 
