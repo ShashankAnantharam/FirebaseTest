adb72f4aafd57f25d518a847950ce90b26f7ef98
Shashank Anantharam <shashank95.a@gmail.com>
2017-10-22 17:11:23 +0530

    Small image working

--- app/src/main/java/com/example/shashank_pc/firebasetest/MapsActivity.java
+++ app/src/main/java/com/example/shashank_pc/firebasetest/MapsActivity.java
@@ -1,5 +1,14 @@
 package com.example.shashank_pc.firebasetest;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffXfermode;
+import android.graphics.Rect;
+import android.graphics.RectF;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 
@@ -7,7 +16,11 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptor;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.CircleOptions;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
@@ -40,7 +53,46 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
         // Add a marker in Sydney and move the camera
         LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        Marker marker= mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+
+        Bitmap img = BitmapFactory.decodeResource(getResources(),R.drawable.future_city);
+
+
+        Bitmap small_img=Bitmap.createScaledBitmap(img, 120, 120, false);
+
+        //       Bitmap round_img= getRoundedRectBitmap(img);
+        //      Bitmap round_img= getCircleBitmap(img);
+
+        marker.setIcon(BitmapDescriptorFactory.fromBitmap(small_img));
+
+     //   mMap.addCircle(new CircleOptions().fillColor(R.drawable.other).center(sydney).radius(15.00));
     }
+
+
+    public static Bitmap getRoundedRectBitmap(Bitmap bitmap) {
+        Bitmap result = null;
+        try {
+            result = Bitmap.createBitmap(200, 200, Bitmap.Config.ARGB_8888);
+            Canvas canvas = new Canvas(result);
+
+            int color = 0xff424242;
+            Paint paint = new Paint();
+            Rect rect = new Rect(0, 0, 200, 200);
+
+            paint.setAntiAlias(true);
+            canvas.drawARGB(0, 0, 0, 0);
+            paint.setColor(color);
+            canvas.drawCircle(50, 50, 50, paint);
+            paint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.SRC_IN));
+            canvas.drawBitmap(bitmap, rect, rect, paint);
+
+        } catch (NullPointerException e) {
+        } catch (OutOfMemoryError o) {
+        }
+        return result;
+    }
+
+
+
 }
